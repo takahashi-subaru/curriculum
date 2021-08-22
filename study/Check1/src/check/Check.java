a2b60a97fadf62d81557670fa7c87f31ede4425d
kohei.takahasi <kou21010@gmail.com>
2021-08-22 13:31:23 +0900

    2-1

--- study/Check1/src/check/Check.java
+++ study/Check1/src/check/Check.java
@@ -1,3 +1,5 @@
+package check;
+
 /**
  * バブルソート
  * チェックテスト-Java1章
@@ -9,25 +11,27 @@ public class Check{
         * 問1
         * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
         */
+        int[] data = {3,1,2,7,5};
+
 
         /*
         * 問2
         * 以下のfor文を完成させなさい
         */
-        for(int i = 0; i < /*ここに記述*/; i++){
+        for(int i = 0; i < data.length/*ここに記述*/; i++){
             System.out.print(data[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < /*ここに記述*/; i++) {
-            for (int j = /*ここに記述*/; j > i; j--) {
+        for (int i = 0; i < data.length/*ここに記述*/; i++) {
+            for (int j = data.length/*ここに記述*/; j > i; j--) {
                 /*
                 * 問3
                 * 以下、配列の添字を入れてソートを完成させなさい
                 */
-                if(data[/*ここに記述*/] > data[/*ここに記述*/]){
-                  int box = data[/*ここに記述*/];
-                  data[/*ここに記述*/] = data[/*ここに記述*/];
-                  data[/*ここに記述*/] = box;
+                if(data[i/*ここに記述*/] > data[j-1/*ここに記述*/]){
+                  int box = data[i/*ここに記述*/];
+                  data[i/*ここに記述*/] = data[j-1/*ここに記述*/];
+                  data[j-1/*ここに記述*/] = box;
                 }
             }
         }

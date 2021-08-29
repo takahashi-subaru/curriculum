c5b7e1e6ddbc94a8391218232f591061a3f47a7d
kohei.takahasi <kou21010@gmail.com>
2021-08-29 21:05:37 +0900

    2-2

--- study/2-2/src/study/Main.java
+++ study/2-2/src/study/Main.java
@@ -16,12 +16,14 @@ public class Main {
 
         // ① 以下のルールに従いUserクラスのインスタンスを生成してください。
         // 変数名:「user」
-         User user = new User("Kohei", 7 ,"hello" + 77);
+         User user = new User("hoge", 123 ,"abc" + 123);
+
 
         // ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
-        user.showN();
-        user.showI();
-        user.showP();
+        user.printAccountInfoN();
+        user.printAccountInfoI();
+        user.printAccountInfoP();
+
 
     }
 }
--- study/2-2/src/study/User.java
+++ study/2-2/src/study/User.java
@@ -20,13 +20,13 @@ class User {
     // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
     // アクセス修飾子:「protected」
     // メソッド名:「printAccountInfo」
-public void showN() {
+    protected void printAccountInfoN() {
     System.out.println("ユーザー名は、 " + this.userName);
     }
-public void showI() {
+    protected void printAccountInfoI() {
     System.out.println("IDは、 " + this.id);
 }
-public void showP() {
+    protected void printAccountInfoP() {
     System.out.println("パスワードは、 " + this.password);
 }
 
